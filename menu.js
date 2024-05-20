4eeadf5d113180dc576920dfb3ad779bced0f350
w3dada <wadada@skiff.com>
2024-05-20 18:19:00 +0300

    11

--- menu.js
+++ menu.js
@@ -2,7 +2,14 @@ document.addEventListener('DOMContentLoaded', function() {
     const mobileMenu = document.getElementById('mobile-menu');
     const menuList = document.querySelector('.my-menu-list');
 
-    mobileMenu.addEventListener('click', function() {
+    mobileMenu.addEventListener('click', function(event) {
+        event.stopPropagation();
         menuList.classList.toggle('active');
+    });
+
+    document.addEventListener('click', function(event) {
+        if (!mobileMenu.contains(event.target) && !menuList.contains(event.target)) {
+            menuList.classList.remove('active');
+        }
     });
 });
