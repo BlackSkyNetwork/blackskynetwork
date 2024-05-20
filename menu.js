a94e216704c2206dc6a826f39e0fb3750b038fbc
w3dada <wadada@skiff.com>
2024-05-20 18:10:36 +0300

    11

--- menu.js
+++ menu.js
@@ -1,6 +1,8 @@
-const mobileMenu = document.getElementById('mobile-menu');
-const menuList = document.querySelector('.my-menu-list');
+document.addEventListener('DOMContentLoaded', function() {
+    const mobileMenu = document.getElementById('mobile-menu');
+    const menuList = document.querySelector('.my-menu-list');
 
-mobileMenu.addEventListener('click', () => {
-    menuList.classList.toggle('active');
+    mobileMenu.addEventListener('click', function() {
+        menuList.classList.toggle('active');
+    });
 });
